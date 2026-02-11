eba3468d71906d86625c81e5aadaed2354769263
Jorink <maxjorink@gmail.com>
2026-02-11 11:13:54 +0100

    Changed font to fix icon allignment

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+static char *font = "MesloLGS Nerd Font Mono:size=12:antialias=true:autohint=true:hintstyle=hintfull";
 static int borderpx = 2;
 
 /*
